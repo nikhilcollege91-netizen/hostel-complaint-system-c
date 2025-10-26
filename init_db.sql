b9b6f0b2e2d1680d3a882669377b216bacc79ac3
nikhilcollege91-netizen <nikhilcollege91@gmail.com>
2025-10-26 09:06:56 +0530

    Delete init_db.sql

--- init_db.sql
+++
@@ -1,6 +0,0 @@
-
-PRAGMA foreign_keys = ON;
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, email TEXT, password TEXT, role TEXT DEFAULT 'student', room TEXT);
-CREATE TABLE IF NOT EXISTS complaints (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, title TEXT, type TEXT, description TEXT, filename TEXT, status TEXT DEFAULT 'Pending', created_at DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id));
-CREATE TABLE IF NOT EXISTS sessions (token TEXT PRIMARY KEY, user_id INTEGER, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id));
-INSERT OR IGNORE INTO users (username,password,role) VALUES ('warden','wardenpass','warden');
